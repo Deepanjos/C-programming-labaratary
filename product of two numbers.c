c2f2c277735a6af21d56f0fd5c1ef0c63b7142f0
Deepanjos <deepanjos2007@gmail.com>
2024-09-30 14:51:12 +0530

    Create product of two numbers.c

---
+++ product of two numbers.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{   
+    
+    float num1,num2;
+    
+    scanf("%f",&num1);
+    scanf("%f",&num2);
+    
+    float mul=num1*num2;
+    printf("procuct of two number is %.2f",mul);
+    
+    return 0;
+}
