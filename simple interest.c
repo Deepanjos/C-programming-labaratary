b0ce537b40b8b40c2aa2991e448437e2de7e0f49
Deepanjos <deepanjos2007@gmail.com>
2024-09-30 15:09:12 +0530

    Create simple interest.c

---
+++ simple interest.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{   
+    
+    float p,r,t
+  
+    printf("Enter principle : ");
+    scanf("%f",&p);
+    printf("\nEnter rate          : ");
+    scanf("%f",&r);
+    printf("\nEnter years       :");
+    scanf("%f",&t
+      
+    float si;
+    si=p*r*t/100;
+  
+    printf("\nEnter interest is %.2f",si);
+    
+    return 0;
+}
